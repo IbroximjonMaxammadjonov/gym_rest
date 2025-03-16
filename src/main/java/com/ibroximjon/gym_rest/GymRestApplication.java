fbcada95bce3b814c8e4ab97962c18daa60c9eb6
Ibroximjon <makhammadjonovibrokhimjon@gmail.com>
2025-03-16 22:50:19 +0500

    initial commit

---
+++ src/main/java/com/ibroximjon/gym_rest/GymRestApplication.java
@@ -0,0 +1,13 @@
+package com.ibroximjon.gym_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymRestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GymRestApplication.class, args);
+    }
+
+}
